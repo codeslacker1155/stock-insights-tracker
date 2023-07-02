cc40cca640671b1a6dd39fcf62e9d5eb5f12cb14
codeslacker1155 <cthyatt710@gmail.com>
2023-07-02 19:37:53 -0400

    update

--- src/App.js
+++ src/App.js
@@ -85,34 +85,34 @@ function App() {
       <div className="tradingview-widget-container">
         <div id="tradingview_4d8c0"></div>
       </div>
-      {stockData && (
-        <div id="company-info">
-          {loading && <p>Loading...</p>}
-          {error && <p className="error-message">{error}</p>}
-          {stockData && (
-            <div>
-              <h2>{stockData.symbol}</h2>
-              <p>Address: {stockData.address}</p>
-              <p>City: {stockData.city}</p>
-              <p>State: {stockData.state}</p>
-              <p>ZIP: {stockData.zip}</p>
-              <p>Country: {stockData.country}</p>
-              <p>Phone: {stockData.phone}</p>
-              <p>Website: {stockData.website}</p>
-              <p>Industry: {stockData.industry}</p>
-              <p>Sector: {stockData.sector}</p>
-              <p>Long Business Summary: {stockData.longBusinessSummary}</p>
-              <p>Full-Time Employees: {stockData.fullTimeEmployees}</p>
-              <p>Audit Risk: {stockData.auditRisk}</p>
-              <p>Board Risk: {stockData.boardRisk}</p>
-              <p>Compensation Risk: {stockData.compensationRisk}</p>
-              <p>Shareholder Rights Risk: {stockData.shareHolderRightsRisk}</p>
-              <p>Overall Risk: {stockData.overallRisk}</p>
-              <p>Governance Epoch Date: {stockData.governanceEpochDate}</p>
-              <p>Compensation As Of Epoch Date: {stockData.compensationAsOfEpochDate}</p>
-            </div>
-          )}
-        </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p className="error-message">{error}</p>
+      ) : (
+        stockData && (
+          <div id="company-info">
+            <h2>{stockData.symbol}</h2>
+            <p>Address: {stockData.address}</p>
+            <p>City: {stockData.city}</p>
+            <p>State: {stockData.state}</p>
+            <p>ZIP: {stockData.zip}</p>
+            <p>Country: {stockData.country}</p>
+            <p>Phone: {stockData.phone}</p>
+            <p>Website: {stockData.website}</p>
+            <p>Industry: {stockData.industry}</p>
+            <p>Sector: {stockData.sector}</p>
+            <p>Long Business Summary: {stockData.longBusinessSummary}</p>
+            <p>Full-Time Employees: {stockData.fullTimeEmployees}</p>
+            <p>Audit Risk: {stockData.auditRisk}</p>
+            <p>Board Risk: {stockData.boardRisk}</p>
+            <p>Compensation Risk: {stockData.compensationRisk}</p>
+            <p>Shareholder Rights Risk: {stockData.shareHolderRightsRisk}</p>
+            <p>Overall Risk: {stockData.overallRisk}</p>
+            <p>Governance Epoch Date: {stockData.governanceEpochDate}</p>
+            <p>Compensation As Of Epoch Date: {stockData.compensationAsOfEpochDate}</p>
+          </div>
+        )
       )}
     </div>
   );

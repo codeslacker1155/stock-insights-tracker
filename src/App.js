410ee628efb1c659fb2cdd813e5817d59e1e5458
codeslacker1155 <cthyatt710@gmail.com>
2023-07-02 13:43:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import StockDataDisplay from './components/StockDataDisplay';
-import CompanyInformation from './components/CompanyInformation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* Remove or modify the logo and default content */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/* Add the stock data display and company information components */}
+      <h1>Stock Insights Tracker</h1>
       <StockDataDisplay />
-      <CompanyInformation />
     </div>
   );
 }
--- src/components/StockDataDisplay.js
+++ src/components/StockDataDisplay.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import api from '../utils/api';
 
 const StockDataDisplay = () => {
   const [stockData, setStockData] = useState(null);
 
   useEffect(() => {
+    // Fetch stock data from API
     const fetchStockData = async () => {
       try {
-        const response = await api.fetchStockData();
-        setStockData(response);
+        const response = await fetch(
+          'https://mboum-finance.p.rapidapi.com/mo/module/?symbol=AAPL&module=asset-profile%2Cfinancial-data%2Cearnings%2Ccashflow-statement%2Cbalance-sheet',
+          {
+            method: 'GET',
+            headers: {
+              'X-RapidAPI-Key': '0rD6UyDj8jmshLwoCdZCWBgBf6pIp1UK2BBjsnb2kK9LFosz4o',
+              'X-RapidAPI-Host': 'mboum-finance.p.rapidapi.com',
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setStockData(data);
+        } else {
+          console.log('Error:', response.status);
+        }
       } catch (error) {
-        console.error('Error fetching stock data:', error);
+        console.log('Error:', error);
       }
     };
 
@@ -19,16 +34,17 @@ const StockDataDisplay = () => {
 
   return (
     <div>
-      <h2>Stock Data</h2>
-      {stockData ? (
+      {stockData && (
         <div>
-          <p>Symbol: {stockData.symbol}</p>
-          <p>Current Price: {stockData.price}</p>
-          <p>Change: {stockData.change}</p>
-          {/* Render additional stock data properties as needed */}
+          <h2>{stockData.assetProfile.companyName}</h2>
+          <p>Address: {stockData.assetProfile.address1}, {stockData.assetProfile.city}, {stockData.assetProfile.state}, {stockData.assetProfile.zip}, {stockData.assetProfile.country}</p>
+          <p>Phone: {stockData.assetProfile.phone}</p>
+          <p>Website: <a href={stockData.assetProfile.website} target="_blank" rel="noopener noreferrer">{stockData.assetProfile.website}</a></p>
+          <p>Industry: {stockData.assetProfile.industry}</p>
+          <p>Sector: {stockData.assetProfile.sector}</p>
+          <p>Description: {stockData.assetProfile.longBusinessSummary}</p>
+          <p>Full-Time Employees: {stockData.assetProfile.fullTimeEmployees}</p>
         </div>
-      ) : (
-        <p>Loading stock data...</p>
       )}
     </div>
   );
